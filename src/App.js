9638b3d53be9d5a83c709557ac2099bf91ef60ee
Benjamin Dehli <benjamin@arkitektum.no>
2019-01-22 16:23:47 +0100

    Add main app component

--- src/App.js
+++ src/App.js
@@ -1,28 +1,45 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import { Provider } from 'react-redux';
+
+import { Route, Switch } from 'react-router'
+import { ConnectedRouter } from 'connected-react-router'
+import configureStore, { history } from './configureStore'
 
 import Posts from './components/Posts';
 import PostForm from './components/PostForm';
 
-import store from './store';
+import SearchResults from './components/SearchResults';
+import MainNavigation from './components/partials/MainNavigation';
+import style from './App.scss';
+
+
+
+const initialState = {};
+const store = configureStore(initialState);
+
 
 class App extends Component {
   render() {
     return (
+
       <Provider store={store}>
-        <div className="App">
-          <header className="App-header">
-            <img src={logo} className="App-logo" alt="logo" />
-            <p>
-              Edit <code>src/App.js</code> and save to reload.
-            </p>
-            <PostForm />
-            <hr />
-            <Posts />
-          </header>
-        </div>
+        <ConnectedRouter history={history}>
+          <div>
+            <MainNavigation />
+            <div className={style.pageContent}>
+              <Switch>
+                <Route exact path="/" render={() => (<SearchResults />)} />
+                <Route exact path="/sok" render={() => (<SearchResults />)} />
+                <Route render={() => (<div>Miss</div>)} />
+                <div className="App">
+                  <header className="App-header">
+                  </header>
+                </div>
+              </Switch>
+            </div>
+          </div>
+        </ConnectedRouter>
       </Provider>
     );
   }
