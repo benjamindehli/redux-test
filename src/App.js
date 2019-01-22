8fb36d02d29f2e8e794e889fd7e34640b16462c3
Benjamin Dehli <benjamin@arkitektum.no>
2019-01-22 17:23:31 +0100

    Remove unused code

--- src/App.js
+++ src/App.js
@@ -1,19 +1,13 @@
 import React, { Component } from 'react';
-import './App.css';
 import { Provider } from 'react-redux';
 
 import { Route, Switch } from 'react-router'
 import { ConnectedRouter } from 'connected-react-router'
 import configureStore, { history } from './configureStore'
 
-import Posts from './components/Posts';
-import PostForm from './components/PostForm';
-
 import SearchResults from './components/partials/SearchResults';
 import MainNavigation from './components/partials/MainNavigation';
 import style from './App.scss';
-
-
 
 const initialState = {};
 const store = configureStore(initialState);
@@ -22,7 +16,6 @@ const store = configureStore(initialState);
 class App extends Component {
   render() {
     return (
-
       <Provider store={store}>
         <ConnectedRouter history={history}>
           <div>
@@ -32,10 +25,6 @@ class App extends Component {
                 <Route exact path="/" render={() => (<SearchResults />)} />
                 <Route exact path="/sok" render={() => (<SearchResults />)} />
                 <Route render={() => (<div>Miss</div>)} />
-                <div className="App">
-                  <header className="App-header">
-                  </header>
-                </div>
               </Switch>
             </div>
           </div>
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,10 +1,8 @@
 import { combineReducers } from 'redux';
 import { connectRouter } from 'connected-react-router'
-import postReducer from './postReducer';
 import searchResultReducer from './searchResultReducer';
 
 export default (history) => combineReducers({
 	router: connectRouter(history),
-	posts: postReducer,
 	searchResults: searchResultReducer
 });
